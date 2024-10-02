609faaf0feff9eda4d88fd52d20b89b7e7371f9d
albertboo9 <booalbert60@gmail.com>
2024-10-02 05:19:58 +0100

    code final

--- k_plus_petits.c
+++ k_plus_petits.c
@@ -1,37 +1,33 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
 
-void echanger(int T[], int a , int b){
+void echanger(int T[], int a, int b) {
     int tmp = T[a];
     T[a] = T[b];
     T[b] = tmp;
-
 }
 
-void k_petit(int T[], int n, int k){
-    int i,j, min;
-    for ( i = 0; i < k ; i++)
-    {
-       
-        for ( j = 0; j < n; i++)
-        {
-           if (T[j] < T[i]){
-                echanger(T, j, i);
-           }
+void k_petit(int T[], int n, int k) {
+    int i, j, min;
+    for (i = 0; i < k; i++) {
+        min = i;
+        for (j = i + 1; j < n; j++) {
+            if (T[j] < T[min]) {
+                min = j;
+            }
         }
-        printf("%d", T[i]);
-        
+        echanger(T, i, min);
     }
-    
 
+    // Print the k smallest elements after sorting
+    for (i = 0; i < k; i++) {
+        printf("%d ", T[i]);
+    }
 }
 
-int main(){
-
-    int T[] = (10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+int main() {
+    int T[10] = {10,9, 8, 7, 6, 5, 4, 3, 2, 1};
 
     k_petit(T, 10, 5);
 
-
-
+    return 0;
 }
