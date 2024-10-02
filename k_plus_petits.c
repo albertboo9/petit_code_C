a443cac119ad1cdf911616877d8772b9b86578f6
albertboo9 <booalbert60@gmail.com>
2024-10-02 05:02:26 +0100

    mise à jour

--- k_plus_petits.c
+++ k_plus_petits.c
@@ -1,21 +1,36 @@
 #include<stdio.h>
 #include<math.h>
 
-void echanger(int a, int b){
+void echanger(int T[], int a , int b){
+    int tmp = T[a];
+    T[a] = T[b];
+    T[b] = tmp;
+
+}
+
+void k_petit(int T[], int n, int k){
+    int i,j, min;
+    for ( i = 0; i < k ; i++)
+    {
+       
+        for ( j = 0; j < n; i++)
+        {
+           if (T[j] < T[i]){
+                echanger(T, j, i);
+           }
+        }
+        printf("%d", T[i]);
+        
+    }
     
 
 }
 
-void tri(int T[], int n){
-
-}
-
-void affihe(int T[], int n){
-
-}
-
-
 int main(){
+
+    int T[] = (10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+
+    k_petit(T, 10, 5);
 
 
 
