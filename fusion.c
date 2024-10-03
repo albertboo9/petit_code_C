dd096741515cacdf69559f1e13e7a5717d056e57
albertboo9 <booalbert60@gmail.com>
2024-10-03 22:47:52 +0100

    algo de fusion

--- fusion.c
+++ fusion.c
@@ -1,9 +1,16 @@
 // B.A.E
+// programme C qui fusionne deux tableaux triés et renvoie un tableau trié
 #include <stdio.h>
+
+int* fusion(int* T, int* L, int n){
+
+    
+
+}
 
 int main(){
 
 
-    
+
     return 0;
 }
