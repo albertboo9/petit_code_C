9e8fa57723e97d47025d46a7cf1268b0ec038317
albertboo9 <booalbert60@gmail.com>
2024-10-03 23:02:43 +0100

    algo de fusion MaJ

--- fusion.c
+++ fusion.c
@@ -2,9 +2,28 @@
 // programme C qui fusionne deux tableaux triés et renvoie un tableau trié
 #include <stdio.h>
 
+void echanger(int T[], int a, int b) {
+    int tmp = T[a];
+    T[a] = T[b];
+    T[b] = tmp;
+}
+
+void insertion(int* T, int n, int x){ // cette procédure insère un nombre x dans un tableau trié de taille n
+
+
+int i=n;
+while (T[i] > x && i>0)
+{
+    i--;
+}
+
+
+
+}
+
 int* fusion(int* T, int* L, int n){
 
-    
+
 
 }
 
