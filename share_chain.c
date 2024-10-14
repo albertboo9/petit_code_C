0aec72f29eaa1d4d28d170cfa27097da3bf94569
albertboo9 <booalbert60@gmail.com>
2024-10-14 13:45:01 +0100

    initialisation des liste

--- share_chain.c
+++ share_chain.c
@@ -1,20 +1,39 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 // des éléments contenus dans la liste  simplement chainée
-typedef struct Element{
+typedef struct Element Element ;
+struct Element{
     int data;
     Element *suivant;
-} Element ;
+};
 
 
 // structure qui contiendra le premier élément de la liste simplement chainée
-typedef struct Liste{
+typedef struct Liste Liste ;
+struct Liste{
     Liste *premier;
-} Liste ;
+};
 
 
 typedef int *Vecteur; 
 
+// initialisation de notre liste chainée
+Liste *initialisation(){
+    Liste *liste = malloc(sizeof(*liste));
+    Element *element  = malloc(sizeof( *element));
+
+    if (liste == NULL || element == NULL){
+        exit(EXIT_FAILURE);
+    }
+
+    element->data = 0;
+    element->suivant = NULL;
+    liste->premier = element;
+
+    return liste;
+
+}
 
 
 int main(){
