dfe0605c5f20a5279e835de81b243bef25ce60c6
albertboo9 <booalbert60@gmail.com>
2024-10-14 14:46:34 +0100

    transfert des élément d'un vecteur vers une liste simplement chainée

--- share_chain.c
+++ share_chain.c
@@ -35,6 +35,27 @@ Liste *initialisation(){
 
 }
 
+// Insertion d'un nouvel élément dans une liste chainée
+void insertion(Liste *liste, int nombre){
+    Element *nouveau = malloc(sizeof(*nouveau));
+
+    if(nouveau == NULL || liste == NULL){
+        exit(EXIT_FAILURE);
+    }
+
+    nouveau->data = nombre;
+    nouveau->suivant = liste->premier;
+    liste->premier = nouveau;
+}
+
+// transfert des éléments d'un vecteur vers une liste simplement chainée
+void *transfert1(Liste *liste, Vecteur vecteur, int n){
+    for (int i = 0; i < n; i++)
+    {
+       insertion(liste, vecteur[i]);
+    }
+    
+}
 
 int main(){
     return 0;
